f1b623f1aa20e1fd345a7a187e143fd9e1ce298a
Amishi <kingraniamishi@gmail.com>
2026-07-14 18:18:08 +0530

    Move database configuration to environment variables

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:2007@localhost:5432/library_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
